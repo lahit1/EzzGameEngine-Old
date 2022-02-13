222a83c2c3d11366a0cf87951555509361196618
lahit1 <70071477+lahit1@users.noreply.github.com>
2022-02-13 12:04:13 +0300

    Add files via upload

---
+++ src/main/java/com/Ezz/Game/Engine/math/Vector3.java
@@ -0,0 +1,52 @@
+package com.Ezz.Game.Engine.math;
+
+import android.annotation.NonNull;
+
+public class Vector3 {
+
+	public float x = 0;
+	public float y = 0;
+	public float z = 0;
+
+	public Vector3(){}
+
+    public Vector3(float x, float y, float z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public void set(float x, float y, float z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public void set(@NonNull Vector3 v){
+		x = v.x;
+		y = v.y;
+		z = v.z;
+	}
+	
+	public void add(float x, float y, float z){
+		this.x += x;
+		this.y += y;
+		this.z += z;
+	}
+	
+	public void multiplication(float x, float y, float z){
+		this.x *= x;
+		this.y *= y;
+		this.z *= z;
+	}
+
+	public Float[] toFloatArray(){
+		Float[] f = {x, y, z};
+		return f;
+	}
+
+	@Override
+	public String toString() {
+		return toFloatArray().toString();
+	}
+}
