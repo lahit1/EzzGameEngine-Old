bf7715370c853c6a4a1eeb42df76fb09b0fc8f83
lahit1 <70071477+lahit1@users.noreply.github.com>
2022-02-13 11:39:14 +0300

    Add files via upload

---
+++ src/main/com/Ezz/Game/Engine/math/Quaternion.java
@@ -0,0 +1,58 @@
+package com.Ezz.Game.Engine.math;
+
+import android.annotation.NonNull;
+
+public class Quaternion {
+
+	public float x = 0;
+	public float y = 0;
+	public float z = 0;
+	public float w = 0;
+
+	public Quaternion(){}
+
+    public Quaternion(float x, float y, float z, float w){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.w = w;
+	}
+
+	public void set(float x, float y, float z, float w){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.w = w;
+	}
+
+	public void set(@NonNull Quaternion q){
+		x = q.x;
+		y = q.y;
+		z = q.z;
+		w = q.w;
+	}
+	
+	public void add(float x, float y, float z, float w){
+		this.x += x;
+		this.y += y;
+		this.z += z;
+		this.w += w;
+	}
+	
+	public void multiplication(float x, float y, float z, float w){
+		this.x *= x;
+		this.y *= y;
+		this.z *= z;
+		this.w *= w;
+	}
+
+	public Float[] toFloatArray(){
+		Float[] f = {x, y, z, w};
+		return f;
+	}
+
+	@Override
+	public String toString() {
+		return toFloatArray().toString();
+	}
+}
