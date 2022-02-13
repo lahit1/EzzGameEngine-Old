be6a5ce1b7175dce5d770de0e1ddb0f9ce20fea3
lahit1 <70071477+lahit1@users.noreply.github.com>
2022-02-13 11:37:48 +0300

    Add files via upload

---
+++ src/main/com/Ezz/Game/Engine/ui/CircleEntity.java
@@ -0,0 +1,26 @@
+package com.Ezz.Game.Engine.ui;
+
+import android.graphics.Canvas;
+import com.Ezz.Game.Engine.Entity;
+import com.Ezz.Game.Engine.Universe;
+
+public class CircleEntity extends Entity {
+	
+	public CircleEntity(Universe universe){
+		super(universe);
+	}
+	
+	public CircleEntity(Universe universe, float radius){
+		super(universe);
+		getSize().set(radius * 2, radius * 2);
+	}
+	
+	public void setRadius(float radius){
+		getSize().set(radius * 2, radius * 2);
+	}
+
+	@Override
+	public void draw(Canvas canvas) {
+		canvas.drawCircle(getRenderX(), getRenderY(), getSize().x / 2, getPaint());
+	}
+}
---
+++ src/main/com/Ezz/Game/Engine/ui/RectEntity.java
@@ -0,0 +1,17 @@
+package com.Ezz.Game.Engine.ui;
+
+import android.graphics.Canvas;
+import com.Ezz.Game.Engine.Entity;
+import com.Ezz.Game.Engine.Universe;
+
+public class RectEntity extends Entity{
+    
+    public RectEntity(Universe universe){
+		super(universe);
+	}
+
+	@Override
+	public void draw(Canvas canvas) {
+		canvas.drawRect(getRenderX(), getRenderY(), getRenderX() + getSize().x, getRenderY() + getSize().y, getPaint());
+	}
+}
